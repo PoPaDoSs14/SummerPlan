9f708a065f1004b571d403c65425356bb3914b7c
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-05-10 20:43:15 +0300

    added TaskRepository

---
+++ app/src/main/java/com/example/summerplan/TaskRepository.kt
@@ -0,0 +1,15 @@
+package com.example.summerplan
+
+import com.example.summerplan.data.database.TaskDao
+import com.example.summerplan.data.model.Task
+
+class TaskRepository(private val taskDao: TaskDao) {
+
+    suspend fun insert(task: Task) {
+        taskDao.insert(task)
+    }
+
+    suspend fun getTasksForPlan(planId: Long): List<Task> {
+        return taskDao.getAllTasks(planId)
+    }
+}
