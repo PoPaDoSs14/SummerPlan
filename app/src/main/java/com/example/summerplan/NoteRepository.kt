c5e62b1baff2b4dc7274c4f35d9596818c7e62a3
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-05-10 20:43:55 +0300

    added NoteRepository

---
+++ app/src/main/java/com/example/summerplan/NoteRepository.kt
@@ -0,0 +1,15 @@
+package com.example.summerplan
+
+import com.example.summerplan.data.database.NoteDao
+import com.example.summerplan.data.model.Note
+
+class NoteRepository(private val noteDao: NoteDao) {
+
+    suspend fun insert(note: Note) {
+        noteDao.insert(note)
+    }
+
+    suspend fun getAllNotes(): List<Note> {
+        return noteDao.getAllNotes()
+    }
+}
