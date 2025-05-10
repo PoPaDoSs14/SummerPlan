50a501a1046214b8bf6ec6285ec680fd9d708df9
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-05-10 20:41:19 +0300

    added PlanRepository

---
+++ app/src/main/java/com/example/summerplan/data/PlanRepository.kt
@@ -0,0 +1,15 @@
+package com.example.summerplan.data
+
+import com.example.summerplan.data.database.PlanDao
+import com.example.summerplan.data.model.Plan
+
+class PlanRepository(private val planDao: PlanDao) {
+
+    suspend fun insert(plan: Plan) {
+        planDao.insert(plan)
+    }
+
+    suspend fun getAllPlans(): List<Plan> {
+        return planDao.getAllPlans()
+    }
+}
