a6892ecf0b5a599b40fa7352401fdd9f783571cc
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-05-11 21:00:05 +0300

    added TaskUseCase

--- app/src/main/java/com/example/summerplan/domain/TaskUseCase.kt
+++ app/src/main/java/com/example/summerplan/domain/TaskUseCase.kt
@@ -1,4 +1,17 @@
 package com.example.summerplan.domain
 
-class TaskUseCase {
+import com.example.summerplan.data.model.Task
+import com.example.summerplan.data.repository.TaskRepository
+
+class TaskUseCase(private val repository: TaskRepository) {
+
+    suspend fun createTask(name: String, description: String, difficulty: Int, planId: Long): Task {
+        val task = Task(name = name, description = description, difficulty = difficulty, planId = planId)
+        repository.insert(task)
+        return task // Возвращаем созданную задачу.
+    }
+
+    suspend fun getTasksForPlan(planId: Long): List<Task> {
+        return repository.getTasksForPlan(planId)
+    }
 }
