9fbb0418e09dac66bd7df620b5bfe01d474d3f65
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-05-16 21:17:34 +0300

    delete PlanViewModel.kt from di

--- app/src/main/java/com/example/summerplan/di/ViewModelModule.kt
+++ app/src/main/java/com/example/summerplan/di/ViewModelModule.kt
@@ -2,7 +2,6 @@ package com.example.summerplan.di
 
 import androidx.lifecycle.ViewModel
 import com.example.summerplan.presentation.NoteViewModel
-import com.example.summerplan.presentation.PlanViewModel
 import com.example.summerplan.presentation.TaskViewModel
 import dagger.Binds
 import dagger.Module
@@ -13,8 +12,6 @@ import dagger.hilt.components.SingletonComponent
 @InstallIn(SingletonComponent::class)
 abstract class ViewModelModule {
 
-    @Binds
-    abstract fun bindPlanViewModel(viewModel: PlanViewModel): ViewModel
 
     @Binds
     abstract fun bindTaskViewModel(viewModel: TaskViewModel): ViewModel
